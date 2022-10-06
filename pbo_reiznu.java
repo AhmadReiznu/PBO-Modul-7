e50b72f57e0481c3f8de69d84789c205b3e7db12
Reiznu Ahmad Tjandrida <55748494+AhmadReiznu@users.noreply.github.com>
2022-10-06 20:04:00 +0700

    Update pbo_reiznu.java

--- pbo_reiznu.java
+++ pbo_reiznu.java
@@ -9,7 +9,7 @@ public class pbo_reiznu {
 }
 
 class Limas{
-    double Sx1; double Sy1; double Sx2; double Sy2; double Px1; double Py1; double Px2; double Py2;
+    double Sx1, Sy1, Sx2, Sy2, Px1, Py1, Px2, Py2;
     Limas(double Sx1,double Sy1, double Sx2, double Sy2, double Px1, double Py1, double Px2, double Py2){
         this.Sx1 = Sx1;
         this.Sy1 = Sy1;
@@ -20,11 +20,13 @@ class Limas{
         this.Py1 = Py1;
         this.Py2 = Py2;
     }
+
     void hitung(){
         double luas_Segitiga, luas_Persegi, luas_Limas;
         System.out.println("=== Data Segitiga ===");
         System.out.println("point\t: " + this.Sx1 + ", " + this.Sy1);
         System.out.println("Sisi Segitiga\t: " + (this.Sy1 + this.Sy2));
+
         luas_Segitiga = 0.5 * (this.Sy1 + this.Sy2) * Math.sqrt(3);
         System.out.println("Luas Segitiga\t: " + luas_Segitiga);
 
@@ -32,12 +34,12 @@ class Limas{
         System.out.println("\n=== Data Persegi ===");
         System.out.println("point\t: " + this.Px1 + ", " + this.Py1);
         System.out.println("Sisi Persegi\t: " + (this.Px1 + this.Px2));
+
         luas_Persegi = (this.Px1 + this.Px2) * (this.Px1 + this.Px2);
         System.out.println("Luas Persegi\t: " + luas_Persegi);
 
         luas_Limas = (4 * luas_Segitiga) + luas_Persegi;
         System.out.println("\n\nLuas Limas : " + luas_Limas);
-
 
     }
 }
